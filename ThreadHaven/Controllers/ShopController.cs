88a32f95660c7a0d2460b1e06079632b4c7c6869
Harmanpreet69 <200567042@student.georgianc.on.ca>
2024-09-10 09:49:01 -0400

    start shop controller

---
+++ ThreadHaven/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThreadHaven.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: shop/ByCategory/123
+        public IActionResult ByCategory(int id)
+        {
+            switch(id)
+            {
+                case 1:
+                    ViewData["Category"] = "Sweaters";
+                    break;
+                case 2:
+                    ViewData["Category"] = "Hoodies";
+                    break;
+                case 3:
+                    ViewData["Category"] = "Pants";
+                    break;
+                case 4:
+                    ViewData["Category"] = "Jeans";
+                    break;
+                default:
+                    return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
